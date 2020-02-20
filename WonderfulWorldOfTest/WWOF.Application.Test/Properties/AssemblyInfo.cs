c116d7d9b5cf3e9e96cf6b6e4e2c67b05492a2ce
Fabian <fabian.guanco@gmail.com>
2020-02-20 20:30:29 -0300

    Creacion de proyecto, agrego biblioteca de data y testunit

---
+++ WonderfulWorldOfTest/WWOF.Application.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WWOF.Application.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WWOF.Application.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5cba8c7b-bb02-40c6-ae27-3ec4752723d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
